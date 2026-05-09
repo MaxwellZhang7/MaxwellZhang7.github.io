9b9505d2e46c838e3bb7683d563f973cfe578ad0
MaxwellZhang7 <maxwellzhg@gmail.com>
2026-05-09 03:54:35 +0000

    Deploying to gh-pages from @ MaxwellZhang7/maxwellzhang7.github.io@0417bd973a5953a72847fd4f9660c22e6742875e 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "MaxwellZhang7/maxwellzhang8",
+    "data-repo": "MaxwellZhang7/maxwellzhang7.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
